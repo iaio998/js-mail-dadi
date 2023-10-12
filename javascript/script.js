3641b3e9253f640c01d17a0effc04004a6a8d8fe
iaio998 <depalatisiacopo@gmail.com>
2023-10-12 16:22:54 +0200

    Working email exercise  (still to be fixed)

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,32 @@
 ///////// ESERCIZIO MAIL
 const emailList = ["abcd@mail.it", "efgh@mail.it", "ilmn@mail.it"];
 const btnVerifyMail = document.querySelector("button");
+const alert = document.querySelector(".alert");
 
 btnVerifyMail.addEventListener("click", function () {
   let userMail = document.getElementById("email").value;
-  let present = false;
-
   let alert = document.getElementById("error");
   let verified = document.getElementById("verified");
   console.log(userMail);
-  if (userMail === "") {
-    alert.classList.remove("d-none");
-    verified.classList.add("d-none");
-  } else {
-    verified.classList.remove("d-none");
-    alert.classList.add("d-none");
+
+  let correct = false;
+  for (let i = 0; i < emailList.length; i++) {
+    let currentMail = emailList[i];
+    if (userMail.toLocaleLowerCase() === currentMail.toLocaleLowerCase()) {
+      correct = true;
+    }
   }
+  let text, cla;
+  if (correct) {
+    cla = "alert-success";
+    text = "Mail presente";
+  } else {
+    cla = "alert-danger";
+    text = "Mail assente";
+  }
+  alert.innerText = text;
+  alert.classList.add(cla);
+  alert.classList.remove("d-none");
 });
 
-// for (let i = 0; i < emailList; i++) {
-//   let currentMail = emailList[i];
-// }
+///////// ESERCIZIO DADI
